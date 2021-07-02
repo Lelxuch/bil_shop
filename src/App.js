27f99de22cca31b87aa5dccb68aa7316ea2d54ad
Lelxuch <nakyp.sanzhar@gmail.com>
2021-07-03 01:58:41 +0600

    day 1
    Added sass structure, header, products main card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,204 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="header-inner">
+          <div className="header-left">
+            <img src="/images/logo.png" alt="" className="header-logo" />
+            <div className="header-text">
+              <div className="header-title">BIL store</div>
+              <div className="header-subtitle">The best choice</div>
+            </div>
+          </div>
+          <nav className="header-nav">
+            <div className="nav-link">
+              <img src="/images/header/cart.svg" alt="" className="nav-img" />
+              <div className="nav-text">198$</div>
+            </div>
+            <div className="nav-link">
+              <img src="/images/header/heart.svg" alt="" className="nav-img" />
+            </div>
+            <div className="nav-link">
+              <img src="/images/header/user.svg" alt="" className="nav-img" />
+            </div>
+          </nav>
+        </div>
       </header>
+      <div className="container">
+        <h1 className="home-title">Products</h1>
+        <div className="home-content">
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="product-main-card">
+            <img src="/images/products/hoodie.png" alt="" className="img" />
+            <div className="title">"Key to life" hoodie</div>
+            <div className="bottom">
+              <div className="price">
+                <div className="price-text">
+                  Price:
+                </div>
+                <div className="price-value">
+                  25$
+                </div>
+              </div>
+              <div className="box-btn plus">
+                <img src="/images/plus.svg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
